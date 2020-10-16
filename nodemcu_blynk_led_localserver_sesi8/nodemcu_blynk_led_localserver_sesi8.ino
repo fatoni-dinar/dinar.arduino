3f61736fac366f9ea45c7528ab79c3c5379a0337
fatoni-dinar <47512800+fatoni-dinar@users.noreply.github.com>
2020-10-16 09:46:29 +0700

    Add files via upload

---
+++ nodemcu_blynk_led_localserver_sesi8/nodemcu_blynk_led_localserver_sesi8.ino
@@ -0,0 +1,72 @@
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+  This example runs directly on ESP8266 chip.
+
+  Note: This requires ESP8266 support package:
+    https://github.com/esp8266/Arduino
+
+  Please be sure to select the right ESP8266 module
+  in the Tools -> Board menu!
+
+  Change WiFi ssid, pass, and Blynk auth token to run :)
+  Feel free to apply it to any other example. It's simple!
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+
+
+char auth[] = "your auth";
+
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "your ssid";
+char pass[] = "pass wifi";
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+  //off pin
+  pinMode(D1,OUTPUT);
+  pinMode(D2,OUTPUT);
+  pinMode(D5,OUTPUT);
+  pinMode(D6,OUTPUT);
+  digitalWrite(D1,1);
+  digitalWrite(D2,1);
+  digitalWrite(D5,1);
+  digitalWrite(D6,1);
+  
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,222), 8080);
+  Blynk.begin(auth, ssid, pass, IPAddress(34,121,180,104), 8080);
+}
+
+void loop()
+{
+  Blynk.run();
+}
